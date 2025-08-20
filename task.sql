1eff669143ed05f49d99c1991c243a225db9f504
Dmytro <demon9709@gmail.com>
2025-08-20 16:57:09 +0300

    Solution2

--- task.sql
+++ task.sql
@@ -1,133 +1,27 @@
-create database ShopDB; 
 
-use ShopDB; 
+select * from Products2 where Name = "AwersomeProduct42";
+# Time: 2025-08-18T21:52:00.650300Z
+# User@Host: root[root] @ localhost []  Id:     8
+# Query_time: 0.001011  Lock_time: 0.000004 Rows_sent: 1  Rows_examined: 60
+SET timestamp=1755553920;
+select * from Products2 where Name = "AwersomeProduct42";
+# Time: 2025-08-18T21:52:01.418519Z
+# User@Host: root[root] @ localhost []  Id:     8
+# Query_time: 0.000546  Lock_time: 0.000002 Rows_sent: 1  Rows_examined: 60
+SET timestamp=1755553921;
+select * from Products2 where Name = "AwersomeProduct42";
+# Time: 2025-08-18T21:52:01.997699Z
+# User@Host: root[root] @ localhost []  Id:     8
+# Query_time: 0.000250  Lock_time: 0.000003 Rows_sent: 1  Rows_examined: 60
+SET timestamp=1755553921;
+select * from Products2 where Name = "AwersomeProduct42";
+# Time: 2025-08-18T21:52:04.704973Z
+# User@Host: root[root] @ localhost []  Id:     8
+# Query_time: 0.000005  Lock_time: 0.000000 Rows_sent: 0  Rows_examined: 60
+SET timestamp=1755553924;
+# administrator command: Quit;
 
--- Create tables 
 
-CREATE TABLE Products1 (
-    ID INT AUTO_INCREMENT,
-    Name VARCHAR(50),
-    PRIMARY KEY (ID)
-) ENGINE=InnoDB;
 
--- Create test data in Products1 table 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct1'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct2'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct3'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct4'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct5'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct6'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct7'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct8'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct9'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct10'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct11'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct12'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct13'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct14'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct15'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct16'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct17'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct18'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct19'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct20'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct21'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct22'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct23'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct24'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct25'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct26'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct27'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct28'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct29'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct30'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct31'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct32'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct33'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct34'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct35'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct36'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct37'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct38'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct39'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct40'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct41'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct42'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct43'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct44'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct45'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct46'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct47'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct48'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct49'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct50'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct51'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct52'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct53'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct54'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct55'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct56'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct57'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct58'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct59'); 
-INSERT INTO Products1 (Name)
-	VALUES ('AwersomeProduct60'); 
+
+The average query speed in the Products2 table turned out to be slower, so I deleted it.
